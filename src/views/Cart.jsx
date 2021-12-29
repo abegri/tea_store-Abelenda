132a11716706d73c9f1fa40b276d78740ef65625
abegri <abelendagrisel.a@gmail.com>
2021-12-29 16:44:34 -0300

    ADD condicional, si el cart está vacío, mostrar mensaje y botón para volver a home

--- src/views/Cart.jsx
+++ src/views/Cart.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { CartConsumer } from '../context/CartProvider'; 
 
 export default function Cart() {
@@ -9,9 +10,23 @@ export default function Cart() {
     return (
         <div>
             <h1>Bienvenido a tu carrito</h1>
-            <ul>
-                <li>{cart.length}</li>
-            </ul>
+            <div>
+                {cart.length === 0 ? 
+                <>
+                    <h2>El carrito está vacío</h2>
+                    <Link to="/" className="continue-link">
+                            <button className="continue-purchase">Volver a la página principal</button>
+                    </Link>
+                   
+                </>
+                 
+                
+                : 
+                
+                <div>{cart.length}</div>
+                
+                }
+            </div>
         </div>
     )
 }
